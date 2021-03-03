d37ba5a6e49e0a7de1925c8f8993f96b6f625822
Demezhan Kazhkenov <59953572+dropdead619@users.noreply.github.com>
2021-03-03 19:45:05 +0600

    Added Encryption Service

--- AuthorizationApp/Program.cs
+++ AuthorizationApp/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-
+using Services;
 namespace AuthorizationApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-          
+           var hashPassword = EncryiptionService.GetHashString("password");
         }
     }
 }
