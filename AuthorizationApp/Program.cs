d74fc33f171e2efa250724f6bc3a54a305fecf17
Demezhan Kazhkenov <59953572+dropdead619@users.noreply.github.com>
2021-03-03 20:47:51 +0600

    Added UI, EmailVailidation, Registration/Authorization

--- AuthorizationApp/Program.cs
+++ AuthorizationApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Models;
 using Services;
 namespace AuthorizationApp
 {
@@ -6,7 +7,61 @@ namespace AuthorizationApp
     {
         static void Main(string[] args)
         {
+           var password = " ";
+           var login = " ";
+           var fullName = " ";
+           var email = " ";
+           var path = " ";
+            
            var hashPassword = EncryiptionService.GetHashString("password");
+            /*Пользователь может сохранить информацию о себе: полное имя, почту (проверять на корректность), 
+                путь на аватарку с раширениями png / jpg / jpeg.*/
+            while (true)
+            {
+                Console.Write("\n1. Зарегистрироваться\n2. Войти\n0. Выход\nВыбор: ");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Пожалуйста, введите Логин");
+                        login = Console.ReadLine();
+                        Console.WriteLine("Придумайте пароль:");
+                        password = Console.ReadLine();
+                        Console.WriteLine("Введите полное имя:");
+                        fullName = Console.ReadLine();
+                        Console.WriteLine("Введите email:");
+                        email = Console.ReadLine();
+                        Console.WriteLine("Введите путь к аватарке:");
+                        path = Console.ReadLine();
+                        var profile = new Profile {FullName = fullName, Email = email, PathToAvatar = path };
+                        if (AuthUtil.Registration(login, password, profile) == true)
+                            {
+                                Console.WriteLine("Вы успешно зарегистрированы!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ошибка регистрации! Введены неверные данные, либо пользователь уже зарегистрирован");
+                            }
+                        break;
+                    case "2":
+                        Console.WriteLine("Пожалуйста, введите ваш Логин");
+                        login = Console.ReadLine();
+                        Console.WriteLine("Введите пароль:");
+                        password = Console.ReadLine();
+                            if (AuthUtil.Authorization(login, password) == true)
+                            {
+                                Console.WriteLine("Вы успешно авторизованы! Для продолжения нажмите любую клавишу");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Введены неверные данные, повторите попытку");
+                                break;
+                            }
+                        break;
+                    case "0":
+                        return;
+                }
+            }
         }
     }
 }
---
+++ Services/AuthUtil.cs
@@ -0,0 +1,55 @@
+﻿using Data;
+using Models;
+
+namespace Services
+{
+    public class AuthUtil
+    {
+        public static bool Authorization(string login, string password)
+        {
+            var userDataAccess = new UserDataAccess();
+            var userList = userDataAccess.Select();
+            foreach (var element in userList)
+            {
+                if (element.Login == login)
+                {
+                   if(element.Password == EncryiptionService.GetHashString(password))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool Registration(string login, string password, Profile profile)
+        {
+            var userDataAccess = new UserDataAccess();
+            var userList = userDataAccess.Select();
+            foreach (var element in userList)
+            {
+                if (element.Login == login)
+                {
+                    return false;
+                }
+            }
+            //Проверяю на валидность Email
+            var isValid = EmailValidationService.IsValidEmail(profile.Email);
+            if (isValid)
+            {
+                var user = new User()
+                {
+                    Login = login,
+                    Password = EncryiptionService.GetHashString(password)
+                };
+                userDataAccess.Insert(user);
+                profileDataAccess.Insert(profile);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
