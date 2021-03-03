0db9b0c8ace2ce08f2205f3d8334c168b245dbd7
Demezhan Kazhkenov <59953572+dropdead619@users.noreply.github.com>
2021-03-03 21:43:54 +0600

    Formats service

---
+++ Services/FileExtensionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+
+namespace Services
+{
+    public class FileExtensionService
+    {
+        public static bool HasExtension(string filename)
+        {
+            // add other possible extensions here
+            return Path.GetExtension(filename).Equals(".jpg", StringComparison.InvariantCultureIgnoreCase)
+                || Path.GetExtension(filename).Equals(".jpeg", StringComparison.InvariantCultureIgnoreCase)
+                || Path.GetExtension(filename).Equals(".png", StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
