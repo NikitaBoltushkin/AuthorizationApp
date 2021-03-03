8ebf9ee30e06aee223749840b8275226a4babaf8
Nikita Boltushkin <russkiykz@gmail.com>
2021-03-03 19:50:50 +0600

    Add DataDb

---
+++ Models/Profile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Profile
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PathToAvatar { get; set; }
+    }
+}
